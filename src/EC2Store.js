e462c282c92e2b7eab078ac9356c1637bd58f492
Sputnik <sputnik@trioxis.com>
2015-12-24 15:33:41 +1100

    Add comments

--- src/EC2Store.js
+++ src/EC2Store.js
@@ -1,12 +1,20 @@
+// Class that gets information from AWS using the AWS Node API.
 class EC2Store {
+	// TODO: Decide if/where credentials are passed in to the class
 	constructor () {
 
 	}
 
+	// A list of all snapshots in the AWS account. Should return a Promise.
+	// TODO: * Do you want to filter out the snapshots you don't need here
+	//	where the request is made or later in the script? Should this return
+	// 	the raw data response or convert it in to a nicer format?
 	listSnapshots () {
 		return Promise.resolve([]);
 	}
 
+	// A list of all EBS volumes in the AWS account. Should return a Promise.
+	// TODO: Same questions as listSnapshots()
 	listEBS () {
 		return new Promise((res) => {res([]);});
 	}
--- src/SnapshotAnalyser.js
+++ src/SnapshotAnalyser.js
@@ -1,7 +1,11 @@
+// Given a list of snapshots, returns a list of snapshots that have expired
+// This can be determined using ExpiryDate tags
 let findDeadSnapshots = (snapshotList) => {
 	return [snapshotList];
 };
 
+// Helper function that checks if a single snapshot has expired or not.
+// Returns true if the snapshot has expired and needs to be deleted. 
 let snapshotIsDead = (snapshot) => {
 	return snapshot;
 };
