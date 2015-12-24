2938f6a10e9d5e243247b0d0b8b00f25f40a345b
Sputnik <sputnik@trioxis.com>
2015-12-24 14:10:41 +1100

    Add more test files

---
+++ test/_TestActionCreator.js
@@ -0,0 +1,18 @@
+import expect from 'expect.js';
+
+import {makeDeleteAction, makeCreateAction} from '../src/ActionCreator';
+
+describe('ActionCreator', () => {
+	describe('makeDeleteAction', () => {
+		it('should create a deletion action for the given snapshot');
+	});
+
+	describe('makeCreateAction', () => {
+		it('should return true if the snapshot is past its expiry date');
+		it('should return false if the snapshot is still within its expiry date');
+	});
+
+	describe('determineBackupsNeeded', () => {
+		it('should return a list of backups needed for the given volume based on recentness of the snapshots in the snaphot list');
+	})
+})
---
+++ test/_TestActioner.js
@@ -0,0 +1,9 @@
+import expect from 'expect.js';
+
+import {doActions} from '../src/Actioner';
+
+describe('Actioner', () => {
+	describe('doActions', () => {
+		it('should return a list of backups needed for the given volume based on recentness of the snapshots in the snaphot list');
+	})
+})
