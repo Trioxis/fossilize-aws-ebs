736cf4ec8cf539ffcf129ef3f1e23b2d74de0791
Sputnik <sputnik@trioxis.com>
2016-01-04 11:38:09 +1100

    Add placeholder test about filtering snapshots relevant to the owner of the account

--- test/_TestEC2Store.js
+++ test/_TestEC2Store.js
@@ -6,6 +6,7 @@ describe('EC2Store', () => {
 	describe('listSnapshots', () => {
 		it('should ask AWS EC2 for a list of all snapshots');
 		it('should return a Promise that resolves to an array of EC2 snapshots');
+		it('should filter snapshots so that only snapshots owned by us ... something')
 	});
 
 	describe('listEBS', () => {
