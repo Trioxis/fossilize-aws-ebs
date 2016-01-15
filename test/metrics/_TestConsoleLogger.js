78c574db9b305684c04d8b99688eb633ba0cf0fe
Sputnik <sputnik@trioxis.com>
2016-01-15 11:08:53 +1100

    Add scaffolded tests

---
+++ test/metrics/_TestConsoleLogger.js
@@ -0,0 +1,7 @@
+import expect from 'expect.js';
+
+describe('ConsoleLogger', () => {
+	describe('log', () => {
+		it('should log the given message to the console');
+	});
+});
---
+++ test/metrics/_TestMetricManager.js
@@ -0,0 +1,13 @@
+import expect from 'expect.js';
+
+describe('MetricManager', () => {
+	describe('log', () => {
+		it.skip('should log to a logging handler that is added to the manager');
+	});
+
+	describe('pushMetric', () => {
+		it.skip('should push metrics to a metric handler that is added to the manager');
+	});
+
+	it.skip('should be able to deal with handlers that expose both log and pushMetric functions')
+});
