b12be4991058e6b039257e602b8512935621404b
Sputnik <sputnik@trioxis.com>
2016-01-15 16:58:34 +1100

    Make main file correctly report when it's done

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var BackupRunner = require('./build/');
 
 console.log('Runnning the backup script in ./build/');
-BackupRunner.default();
-console.log('Done, exiting');
+BackupRunner.default()
+	.then(() => {
+		console.log('Done, exiting');
+});
