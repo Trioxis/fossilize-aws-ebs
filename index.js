9823d1b887343f7e8d1792f249226aa309aa7986
Sputnik <sputnik@trioxis.com>
2016-01-20 10:03:12 +1100

    Fix indenting error and print BackupRunner output

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var BackupRunner = require('./build/');
 
 console.log('Runnning the backup script in ./build/');
-BackupRunner.default()
-	.then(() => {
-		console.log('Done, exiting');
+BackupRunner.default().then((result) => {
+	console.log('DONE');
+	console.log('Printing output:');
+	console.log(result);
+	console.log('Exiting');
 });
