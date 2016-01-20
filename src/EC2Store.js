3a9b8f55b8f65da89c4590acc0bb23fb3079ee3c
Sherwesh <sbajracharya@trioxis.com>
2016-01-20 16:20:21 +1100

    Implemented list.Snapshots array resolution, accounting for conditions when Name or ExpiryDate aren't at fixed indices in the array. Implemented list.Snapshots array filtering to only return snapshots with the tag 'backups:config-v0'.

--- src/EC2Store.js
+++ src/EC2Store.js
@@ -22,16 +22,59 @@ class EC2Store {
 			let ec2 = new AWS.EC2();
 			ec2.describeSnapshots({}, function (err, response) {
 				if (err) reject(err);
-				else resolve(response.Snapshots.map(function(obj){
-					var obj2 = {
-						SnapshotId: obj.SnapshotId,
-						StartTime: obj.StartTime,
-						Name: obj.Tags[0].Value,
-						ExpiryDate: parseInt(obj.Tags[1].Value.slice(11,23))
-					};
-					return obj2;
-				})
+
+				else {
+					var taggedForBackup = response.Snapshots.filter(function(obj){
+						for (var i=0; i<obj.Tags.length; i++) {
+							if (obj.Tags[i].Key === 'backups:config-v0') {
+								return true;
+							}
+						}
+						return false;
+					});
+
+					resolve(taggedForBackup.map(function(obj){
+
+						var filteredForName = obj.Tags.filter(function(obj){
+							if (obj.Key === 'Name'){
+								return true;
+							} else {
+								return false;
+							}
+						});
+
+						if (filteredForName.length === 1) {
+							filteredForName = filteredForName[0];
+						} else {
+							throw new Error('expected to receive snapshot with a single value for name but length > 1');
+						}
+
+						var filteredForDate = obj.Tags.filter(function(obj){
+							if (obj.Value.slice(0,10) === 'ExpiryDate'){
+								return true;
+							} else {
+								return false;
+							}
+						});
+
+						if (filteredForDate.length === 1) {
+							filteredForDate = parseInt(filteredForDate[0].Value.slice(11,23));
+						} else {
+							throw new Error('expected to receive snapshot with a single value for expiry date but length > 1');
+						}
+
+						var obj2 = {
+							SnapshotId: obj.SnapshotId,
+							StartTime: obj.StartTime,
+							[filteredForName.Key]: filteredForName.Value,
+							ExpiryDate: filteredForDate
+						};
+
+						console.log(obj2);
+						return obj2;
+					})
 			);
+				}
 			});
 		});
 	}
