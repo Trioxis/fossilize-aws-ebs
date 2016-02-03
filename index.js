5c0ee8ef93205c0711080d1fe40e5944c052a4e3
Sputnik <sputnik@trioxis.com>
2016-02-03 12:04:21 +1100

    Remove most logging

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var BackupRunner = require('./build/');
 
 console.log('Runnning the backup script in ./build/');
+console.log();
 BackupRunner.default().then((result) => {
+	console.log();
 	console.log('DONE');
-	console.log('Printing output:');
-	console.log(result);
+	// console.log('Printing output:');
+	// console.log(result);
 	console.log('Exiting');
 });
