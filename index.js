72dc4c7f6e1d6ea59b43c95164415dff188e0415
Sputnik <sputnik@trioxis.com>
2016-02-03 13:11:54 +1100

    Get entrypoint to leave some space in the log

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ var BackupRunner = require('./build/');
 
 console.log('Runnning the backup script in ./build/');
 console.log();
-BackupRunner.default().then((result) => {
+BackupRunner.default().then(() => {
 	console.log();
-	console.log('DONE');
-	// console.log('Printing output:');
-	// console.log(result);
-	console.log('Exiting');
+	console.log('Done, Exiting');
 });
