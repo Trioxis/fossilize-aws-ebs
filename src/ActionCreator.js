1c421b5f6e8bb7fea459bd3f0d8ea7dd3d82c4d4
Sputnik <sputnik@trioxis.com>
2016-02-04 21:51:35 +1100

    Match snapshots with their respective volumes

--- src/ActionCreator.js
+++ src/ActionCreator.js
@@ -7,18 +7,36 @@ let makeDeleteAction = (snap) => {
 // Given an EBS volume and a list of snapshots, return a list of actions
 // to create the necessary snapshots that fulful the backup requirements
 let makeCreationActions = (volume, snapList) => {
+	// volume = attachSnapsToVolume(volume, snapList);
 
-	let x = snapList.filter((snap) => {
-		// console.log(snap);
-		if (snap.FromVolumeName === volume.Name) {
-			return true;
-		} else {
-			return false
-		}
-	}).length;
-	console.log(`${volume.VolumeId} ${volume.Name} has ${x} related snapshots`);
+	console.log(`(${volume.VolumeId}): '${volume.Name}' has ${volume.Snapshots} related snapshots`);
 	return determineBackupsNeeded(volume, snapList).map(backup => backup);
 };
+
+// Given a list of EBS volumes and list of snapshots, attaches the snapshot to the
+// EBS's Snapshots[BackupType] array. Returns an object containing the volume list
+// and a list of orphaned snapshots
+var matchSnapsToVolumes = (volumes, snapList) => {
+
+	let matchedVolumes = volumes.map((volume) => {
+		volume.Snapshots = {}
+		snapList = snapList.filter((snap) => {
+			// console.log(snap);
+			if (snap.FromVolumeName === volume.Name) {
+				if (!volume.Snapshots[snap.BackupType]) volume.Snapshots[snap.BackupType] = [];
+				volume.Snapshots[snap.BackupType].push(snap);
+				return false;
+			} else {
+				return true;
+			}
+		});
+		return volume;
+
+	});
+
+	let orphanedSnaps = snapList;
+	return {matchedVolumes, orphanedSnaps};
+}
 
 // Read the tags on the EBS volume and check if the backup requirements are
 // fulfilled by the current state of snapshots. For example: if the volume
@@ -28,4 +46,4 @@ let determineBackupsNeeded = (volume, snapList) => {
 	return [{volume, snapList}];
 };
 
-export {makeDeleteAction, makeCreationActions, determineBackupsNeeded};
+export {makeDeleteAction, matchSnapsToVolumes, makeCreationActions, determineBackupsNeeded};
