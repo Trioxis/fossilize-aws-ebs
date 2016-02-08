15c1507d05f52edabdb933efd2221e5f90294a4b
Sputnik <sputnik@trioxis.com>
2016-02-08 11:26:26 +1100

    Test snapshot sorting function

--- test/_TestAnalyser.js
+++ test/_TestAnalyser.js
@@ -59,3 +59,67 @@ describe('matchSnapsToVolumes', () => {
 		}])
 	});
 });
+
+
+describe('sortSnapsByMostRecent', () => {
+	it('should sort an array of snapshots from most to least recently created', () => {
+		let now = moment();
+		let snapList = [
+			{
+				Name: 'first',
+				SnapshotId: 'snap-00000001',
+				FromVolumeId: 'vol-abcdabcd',
+				FromVolumeName: 'volume-lol',
+				StartTime: moment().subtract(1, 'hours'),
+				ExpiryDate: now,
+				BackupType: 'Daily'
+			},
+			{
+				Name: 'second',
+				SnapshotId: 'snap-00000002',
+				FromVolumeId: 'vol-abcdabcd',
+				FromVolumeName: 'volume-lol',
+				StartTime: moment().subtract(9, 'hours'),
+				ExpiryDate: now,
+				BackupType: 'Daily'
+			},
+			{
+				Name: 'third',
+				SnapshotId: 'snap-00000003',
+				FromVolumeId: 'vol-abcdabcd',
+				FromVolumeName: 'volume-lol',
+				StartTime: moment().subtract(1, 'days'),
+				ExpiryDate: now,
+				BackupType: 'Daily'
+			},
+			{
+				Name: 'fourth',
+				SnapshotId: 'snap-00000004',
+				FromVolumeId: 'vol-abcdabcd',
+				FromVolumeName: 'volume-lol',
+				StartTime: moment().subtract(1, 'months'),
+				ExpiryDate: now,
+				BackupType: 'Daily'
+			},
+			{
+				Name: 'last',
+				SnapshotId: 'snap-00000005',
+				FromVolumeId: 'vol-abcdabcd',
+				FromVolumeName: 'volume-lol',
+				StartTime: moment().subtract(7, 'years'),
+				ExpiryDate: now,
+				BackupType: 'Daily'
+			},
+		];
+
+		let sortedList = sortSnapsByMostRecent([
+			snapList[2],
+			snapList[4],
+			snapList[1],
+			snapList[0],
+			snapList[3]
+		]);
+
+		expect(sortedList).to.be.eql(snapList);
+	});
+});
