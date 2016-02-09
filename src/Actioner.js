85df5ee8bd44b1200e196f669f93a2aac07d46cf
Sputnik <sputnik@trioxis.com>
2016-02-09 12:35:16 +1100

    Snapshot volumes with a catch method that retries if too many snapshot requests were made

--- src/Actioner.js
+++ src/Actioner.js
@@ -1,39 +1,51 @@
+import AWS from 'aws-sdk';
+AWS.config.update({region: 'ap-southeast-2'});
+
+let promiseToPauseFor = (ms) => new Promise(resolve => {
+	setTimeout(resolve, ms);
+});
+
 // Given an array of actions, perform the actions in AWS EC2
 // These could be creating a snapshot from an EBS volume, or
 // deleting an existing snapshot
 let doActions = (actions) => {
-	let promises = [];
-	let warnings = [];
-	actions.map((action) => {
-		switch (action.Action) {
-			case 'SNAPSHOT_VOLUME':
+	// let results = [];
+	// let warnings = [];
+	// let promises = Promise.resolve();
+	let ec2 = new AWS.EC2();
 
-				promises.push(createSnapshotPromise(action));
-				break;
-			default:
-				warnings.push(`Unknow action type ${action.Action}`);
-				break;
-		}
-	});
+	return Promise.all(
+		actions.map((action) => {
+			switch (action.Action) {
+				case 'SNAPSHOT_VOLUME':
+					let makeSnapPromise = () => {
+						return new Promise((resolve, reject) => {
+							// throw new Error('OmgBbqError');
+							ec2.createSnapshot({ DryRun: false, VolumeId: action.VolumeId, Description: `${action.BackupType} omg`}, (err, res) => {
+								if (err) reject(err);
+								else resolve(res);
+							});
+						}).catch((err) => {
+							if (err.code === 'SnapshotCreationPerVolumeRateExceeded') {
+								// Cannot create a snapshot on the same volume more than one every 15 seconds
+								// Wait and try again
+								return promiseToPauseFor(15000).then(() => makeSnapPromise());
+							} else {
+								console.log(`Error on ${action.BackupType}`);
+								console.log(err);
+								return Promise.resolve(err);
+							}
+						});
+					};
 
+					return makeSnapPromise();
+				default:
+					console.log(`Unknown action type ${action.Action}`);
+					return Promise.resolve(null);
 
-	return Promise.all(promises)
-		.then(resolved => ({results: resolved, warnings: warnings}));
-};
-
-// Makes a promise that always resolves
-let createSnapshotPromise = (action) => {
-	return new Promise((resolve, reject) => {
-		resolve(action);
-		reject(action);
-		// make snapshot and tag it
-		// then, if all goes well, resolve with an action result that explains
-		// the operation was successful
-	}).catch((error) => {
-		return error;
-		// convert the promise in to an action result that explains the error
-		// and return a resolved Promise
-	});
+			}
+		})
+	);
 };
 
 
