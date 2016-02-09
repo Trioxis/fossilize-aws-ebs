1f43c0c0a0121656726da29a142bfb719a230033
Sputnik <sputnik@trioxis.com>
2016-02-09 15:12:02 +1100

    Make snapshot promise recreate itself if it fails and has to wait

--- src/Actioner.js
+++ src/Actioner.js
@@ -1,5 +1,6 @@
 import AWS from 'aws-sdk';
 AWS.config.update({region: 'ap-southeast-2'});
+let ec2 = new AWS.EC2();
 
 let promiseToPauseFor = (ms) => new Promise(resolve => {
 	setTimeout(resolve, ms);
@@ -12,33 +13,14 @@ let doActions = (actions) => {
 	// let results = [];
 	// let warnings = [];
 	// let promises = Promise.resolve();
-	let ec2 = new AWS.EC2();
 
 	return Promise.all(
 		actions.map((action) => {
 			switch (action.Action) {
 				case 'SNAPSHOT_VOLUME':
-					let makeSnapPromise = () => {
-						return new Promise((resolve, reject) => {
-							// throw new Error('OmgBbqError');
-							ec2.createSnapshot({ DryRun: false, VolumeId: action.VolumeId, Description: `${action.BackupType} omg`}, (err, res) => {
-								if (err) reject(err);
-								else resolve(res);
-							});
-						}).catch((err) => {
-							if (err.code === 'SnapshotCreationPerVolumeRateExceeded') {
-								// Cannot create a snapshot on the same volume more than one every 15 seconds
-								// Wait and try again
-								return promiseToPauseFor(15000).then(() => makeSnapPromise());
-							} else {
-								console.log(`Error on ${action.BackupType}`);
-								console.log(err);
-								return Promise.resolve(err);
-							}
-						});
-					};
 
-					return makeSnapPromise();
+
+					return makeBackupPromise(action);
 				default:
 					console.log(`Unknown action type ${action.Action}`);
 					return Promise.resolve(null);
@@ -46,6 +28,36 @@ let doActions = (actions) => {
 			}
 		})
 	);
+};
+
+let makeBackupPromise = (action) => {
+	return makeSnapshotPromise(action)
+	.catch(err => salvagePromise(err, action));
+};
+
+let makeSnapshotPromise = (action) => {
+	return new Promise((resolve, reject) => {
+		ec2.createSnapshot({ DryRun: false, VolumeId: action.VolumeId, Description: `${action.BackupType} omg`}, (err, res) => {
+			if (err) reject(err);
+			else {
+				console.log(`Snapshotted ${action.VolumeName} for ${action.BackupType}`);
+				resolve(res);
+			}
+		});
+	});
+};
+
+let salvagePromise = (err, action) => {
+	// Attempt to
+	if (err.code === 'SnapshotCreationPerVolumeRateExceeded') {
+		// Cannot create a snapshot on the same volume more than one every 15 seconds
+		// Wait and try again
+		return promiseToPauseFor(15000).then(() => makeBackupPromise(action));
+	} else {
+		console.log(`Error on ${action.BackupType}`);
+		console.log(err);
+		return Promise.reject(err);
+	}
 };
 
 
