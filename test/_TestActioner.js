2ef3e32b7a8d894e19003a1301230b2fc5d65f77
Sputnik <sputnik@trioxis.com>
2016-02-11 12:12:32 +1100

    Individually test units and reduce coverage

--- test/_TestActioner.js
+++ test/_TestActioner.js
@@ -21,7 +21,7 @@ describe('Actioner', () => {
 		sandbox.restore();
 	});
 
-	describe('makeBackup', () => {
+	describe('_makeSnapshot', () => {
 		it('should attempt to snapshot the volume in the action provided', () => {
 			let action = {
 				Action: 'SNAPSHOT_VOLUME',
@@ -34,7 +34,7 @@ describe('Actioner', () => {
 			mockEC2.createSnapshot = sinon.stub().yields(null, {SnapshotId: 'snap-abcd1234'});
 			mockEC2.createTags = sinon.stub().yields(null);
 
-			return actioner.makeBackup(action)
+			return actioner._makeSnapshot(action)
 				.then(() => {
 					expect(mockEC2.createSnapshot.called).to.be.ok();
 					expect(mockEC2.createSnapshot.args[0][0]).to.eql({
@@ -44,37 +44,42 @@ describe('Actioner', () => {
 					return;
 				});
 		});
-
-		it.skip('should retry after 15 seconds if a SnapshotCreationPerVolumeRateExceeded error is returned', () => {
-			let action = {
-				Action: 'SNAPSHOT_VOLUME',
-				VolumeId: 'vol-1234abcd',
-				VolumeName: 'a-volume',
-				BackupType: 'Hourly',
-				ExpiryDate: moment().add('24', 'hours')
-			};
-
-			mockEC2.createSnapshot = sinon.stub()
-			mockEC2.createSnapshot.yields(null, {SnapshotId: 'yey'})
-			mockEC2.createSnapshot.onFirstCall().yields({
-					message: 'The maximum per volume CreateSnapshot request rate has been exceeded. Use an increasing or variable sleep interval between requests.',
-				  code: 'SnapshotCreationPerVolumeRateExceeded',
-				  time: 'Thu Feb 11 2016 10:41:37 GMT+1100 (AEDT)',
-				  requestId: '8b0b0101-9848-4d80-b012-ef16c4347182',
-				  statusCode: 400,
-				  retryable: false,
-				  retryDelay: 30
-				}, null);
-
-
-
-			mocks._promiseToPauseFor = sandbox.stub(actioner, '_promiseToPauseFor').returns(Promise.resolve());
-
-			return actioner.makeBackup(action)
-				.then(() => {
-					expect
-					return;
-				})
-		});
 	});
+
+	describe('_tagSnapshot', () => {
+		it('should tag the given snapshot appropriately based on the action it is given');
+	});
+
+});
+
+it.skip('should retry after 15 seconds if a SnapshotCreationPerVolumeRateExceeded error is returned', () => {
+	let action = {
+		Action: 'SNAPSHOT_VOLUME',
+		VolumeId: 'vol-1234abcd',
+		VolumeName: 'a-volume',
+		BackupType: 'Hourly',
+		ExpiryDate: moment().add('24', 'hours')
+	};
+
+	mockEC2.createSnapshot = sinon.stub()
+	mockEC2.createSnapshot.yields(null, {SnapshotId: 'yey'})
+	mockEC2.createSnapshot.onFirstCall().yields({
+		message: 'The maximum per volume CreateSnapshot request rate has been exceeded. Use an increasing or variable sleep interval between requests.',
+		code: 'SnapshotCreationPerVolumeRateExceeded',
+		time: 'Thu Feb 11 2016 10:41:37 GMT+1100 (AEDT)',
+		requestId: '8b0b0101-9848-4d80-b012-ef16c4347182',
+		statusCode: 400,
+		retryable: false,
+		retryDelay: 30
+	}, null);
+
+
+
+	mocks._promiseToPauseFor = sandbox.stub(actioner, '_promiseToPauseFor').returns(Promise.resolve());
+
+	return actioner.makeBackup(action)
+	.then(() => {
+		expect
+		return;
+	})
 });
