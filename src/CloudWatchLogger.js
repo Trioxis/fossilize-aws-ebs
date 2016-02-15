db150de72ec8633a8d16684d1bdfdf51fa93e6cb
Sputnik <sputnik@trioxis.com>
2016-02-15 17:36:44 +1100

    Add methods to CloudWatchLogger to capture all output sent to console.log and send it to CloudWatch
    This is bad and I should feel bad

--- src/CloudWatchLogger.js
+++ src/CloudWatchLogger.js
@@ -1,4 +1,51 @@
 import AWS from 'aws-sdk';
+import util from 'util';
+
+let consoleLogOverride = false;
+let logs = [];
+let collectConsoleLog = () => {
+	if (!consoleLogOverride) {
+		(function () {
+			consoleLogOverride = true;
+			var oldLog = console.log;
+			console.log = function() {
+				if (arguments['0']) {
+					logs.push({
+						message: util.format(arguments['0']),
+						timestamp: Date.now()
+					});
+				}
+				oldLog.apply(console, arguments);
+			};
+		})();
+	}
+};
+
+let dumpConsoleLogToCloudWatch = (nextToken) => {
+	let pushedLogs = logs;
+	logs = [];
+	var cloudwatchlogs = new AWS.CloudWatchLogs();
+	return new Promise((resolve, reject) => {
+		cloudwatchlogs.putLogEvents({
+			logEvents: pushedLogs,
+			logGroupName: 'tmp-1',
+			logStreamName: 'tmp-strm-2',
+			sequenceToken: nextToken
+		}, (err, data) => {
+			if (err) {
+				logs = logs.concat(pushedLogs);
+				if (err.code === 'InvalidSequenceTokenException') {
+					let seq = err.message.match(/\d+/)[0];
+					resolve(dumpConsoleLogToCloudWatch(seq));
+				} else {
+					reject(err);
+				}
+			} else {
+				resolve(data);
+			}
+		});
+	});
+};
 
 let logToCloudWatch = (obj) => {
 	return tryPut(obj);
@@ -30,4 +77,4 @@ let tryPut = (obj, nextToken) => {
 	});
 };
 
-export {logToCloudWatch};
+export {logToCloudWatch, collectConsoleLog, dumpConsoleLogToCloudWatch};
